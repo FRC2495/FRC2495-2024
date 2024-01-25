c1ab7a7d57a375a1a2f42a419b14c5eb7b8cf3df
frc2495 <frc2495@gmail.com>
2024-01-24 19:55:05 -0500

    added sp3 three note path variables

--- src/main/java/frc/robot/auton/AutonConstants.java
+++ src/main/java/frc/robot/auton/AutonConstants.java
@@ -5,8 +5,8 @@ public class AutonConstants {
     // Fixed auton distances
 
     // All Starting Position Distances
-    public static final double DISTANCE_TO_PICKUP_NOTE_METERS = 5; //todo fix
-    public static final double DISTANCE_FROM_STARTING_POSITION_TO_LEAVE_COMMUNITY_METERS = 20; //todo fix
+    public static final double DISTANCE_TO_PICKUP_NOTE_METERS = 5; // todo fix
+    public static final double DISTANCE_FROM_STARTING_POSITION_TO_LEAVE_COMMUNITY_METERS = 20; // todo fix
 
     // Starting Position One = Three Note Distances (all in meters prob)
     public static final double STARTING_POSITION_1_X_VALUE = 1.05;
@@ -27,11 +27,24 @@ public class AutonConstants {
     public static final double DISTANCE_FROM_STARTING_POSITION_2_TO_SECOND_NOTE_PICKUP_Y = 5.57;
     public static final double DISTANCE_FROM_SHOOT_SECOND_NOTE_TO_LEAVE_X = 3.84;
 
+    // Starting Position Three = Three Note Distances (all in meters)
+    public static final double STARTING_POSITION_3_X_VALUE = 1.14;
+    public static final double STARTING_POSITION_3_Y_VALUE = 4.17;
+    public static final double DISTANCE_FROM_STARTING_POSITION_3_TO_SHOOT_PRELOAD_X = 2.08;
+    public static final double DISTANCE_FROM_STARTING_POSITION_3_TO_SHOOT_PRELOAD_Y = 4.17;
+    public static final double DISTANCE_FROM_SHOOT_PRELOAD_TO_SECOND_NOTE_PICKUP_X = 2.45;
+    public static final double DISTANCE_FROM_SHOOT_PRELOAD_TO_SECOND_NOTE_PICKUP_Y = 4.06;
+    public static final double DISTANCE_FROM_SECOND_NOTE_PICKUP_TO_SHOOT_SECOND_NOTE_X = 2.08;
+    public static final double DISTANCE_FROM_SECOND_NOTE_PICKUP_TO_SHOOT_SECOND_NOTE_Y = 4.17;
+    public static final double DISTANCE_FROM_SHOOT_SECOND_NOTE_TO_THIRD_NOTE_PICKUP_X = 7.76;
+    public static final double DISTANCE_FROM_SHOOT_SECOND_NOTE_TO_THIRD_NOTE_PICKUP_Y = 0.74;
+    public static final double DISTANCE_FROM_THIRD_NOTE_PICKUP_TO_SHOOT_X = 2.17;
+    public static final double DISTANCE_FROM_THIRD_NOTE_PICKUP_TO_SHOOT_Y = 4.02;
+
 
     // Fixed auton angles
 
     // Starting Position One + Six(?) Angles
     public static final int ANGLE_BETWEEN_CONE_NODE_AND_AREA_AFTER_FIRST_TURN = 180;
-
 
 }
---
+++ src/main/java/frc/robot/auton/sp3/StartingPositionThreeThreeNote.java
@@ -0,0 +1,78 @@
+package frc.robot.auton.sp3;
+
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import frc.robot.RobotContainer;
+import frc.robot.auton.AutonConstants;
+import frc.robot.auton.common.*;
+import frc.robot.commands.drivetrain.*;
+import frc.robot.commands.shooter.*;
+import frc.robot.commands.mouth.*;
+import frc.robot.subsystems.*;
+import frc.robot.auton.sp1.*;
+
+
+// GP = game piece
+// Can be used to place one cube or one cone and either starting position one or two
+public class StartingPositionThreeThreeNote extends SequentialCommandGroup {
+
+    public StartingPositionThreeThreeNote(RobotContainer container, Elevator elevator, Drawer drawer, SwerveDrivetrain drivetrain, Roller roller, Shooter shooter, Neck neck, Mouth mouth){
+
+        addCommands(
+
+            new DrivetrainSwerveRelative(drivetrain, container, createShootPreloadTrajectory(container)),
+
+            new ShooterTimedShoot(shooter, 0.5),
+
+            new StartingPositionOnePickupSecondNote(container, drivetrain, roller),
+            
+            new ShooterTimedShoot(shooter, 0.5), // will have to change in some way to compensate for the distance
+
+            new StartingPositionOnePickupThirdNote(container, drivetrain, roller),
+
+            new DrivetrainSwerveRelative(drivetrain, container, createShootSecondNoteTrajectory(container)),
+
+			new ShooterTimedShoot(shooter, 0.5) // will have to change in some way to compensate for the distance
+
+        ); 
+  
+    }
+
+    public Trajectory createShootPreloadTrajectory(RobotContainer container) {
+		// An example trajectory to follow. All units in meters.
+		Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+			// Start at the origin facing the -X direction
+			new Pose2d(AutonConstants.STARTING_POSITION_1_X_VALUE, AutonConstants.STARTING_POSITION_1_Y_VALUE, Rotation2d.fromDegrees(180.0)),
+			// Pass through these waypoints
+			List.of(),
+			// End straight ahead of where we started, facing forward
+			new Pose2d(AutonConstants.DISTANCE_FROM_STARTING_POSITION_1_TO_AREA_TO_SHOOT_PRELOAD_X, AutonConstants.DISTANCE_FROM_STARTING_POSITION_1_TO_AREA_TO_SHOOT_PRELOAD_Y, Rotation2d.fromDegrees(120)),
+            container.createReverseTrajectoryConfig());
+
+		return trajectory;
+	}
+    
+    public Trajectory createShootSecondNoteTrajectory(RobotContainer container) {
+		// An example trajectory to follow. All units in meters.
+		Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+			// Start at the origin facing the -X direction
+			new Pose2d(AutonConstants.DISTANCE_FROM_SECOND_NOTE_PICKUP_TO_THIRD_NOTE_PICKUP_X, AutonConstants.DISTANCE_FROM_SECOND_NOTE_PICKUP_TO_THIRD_NOTE_PICKUP_Y, Rotation2d.fromDegrees(180)),
+			// Pass through these waypoints
+			List.of(),
+			// End straight ahead of where we started, facing forward
+			new Pose2d(AutonConstants.DISTANCE_FROM_THIRD_NOTE_PICKUP_TO_SHOOT_THIRD_NOTE_X, AutonConstants.DISTANCE_FROM_THIRD_NOTE_PICKUP_TO_SHOOT_THIRD_NOTE_Y, Rotation2d.fromDegrees(155)),
+            container.createReverseTrajectoryConfig());
+
+		return trajectory;
+	}
+
+
+
+}
