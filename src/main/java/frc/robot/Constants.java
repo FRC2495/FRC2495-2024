8059a8fc0a169f1a37a695ced6feffd19b37d994
Mr. A <GabrielArtaud@comcast.net>
2024-02-29 14:54:25 -0500

    Update Constants.java
    fixed the name of the AprilTag locations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,23 +28,23 @@ public final class Constants {
 
 	public static class APRILTAGS {
 
-		// in relation to standing at (0,0) and facing the red speaker
-		public static final int LEFT_BLUE_SOURCE = 1; 
-		public static final int RIGHT_BLUE_SOURCE = 2;
+		// facing the field elements (or from the driver station for the stages)
+		public static final int RIGHT_BLUE_SOURCE = 1; 
+		public static final int LEFT_BLUE_SOURCE = 2;
 		public static final int RIGHT_RED_SPEAKER = 3;
-		public static final int MIDDLE_RED_SPEAKER = 4;
-		public static final int RED_AMP = 5;
-		public static final int BLUE_AMP = 6;
-		public static final int MIDDLE_BLUE_SPEAKER = 7;
-		public static final int RIGHT_BLUE_SPEAKER = 8;
-		public static final int LEFT_BLUE_SOURCET_RED_SOURCE = 9;
-		public static final int RIGHT_RED_SOURCE = 10;
-		public static final int RIGHT_RED_TRAP = 11;
-		public static final int LEFT_RED_TRAP = 12;
-		public static final int CENTER_RED_TRAP = 13;
-		public static final int CENTER_BLUE_TRAP = 14;
-		public static final int LEFT_BLUE_TRAP = 15;
-		public static final int RIGHT_BLUE_TRAP = 16;
+		public static final int MIDDLE_RED_SPEAKER = 4; // target
+		public static final int RED_AMP = 5; // target
+		public static final int BLUE_AMP = 6; // target
+		public static final int MIDDLE_BLUE_SPEAKER = 7; // target
+		public static final int LEFT_BLUE_SPEAKER = 8;
+		public static final int RIGHT_RED_SOURCE = 9;
+		public static final int LEFT_RED_SOURCE = 10;
+		public static final int STAGE_LEFT_RED_TRAP = 11;
+		public static final int STAGE_RIGHT_RED_TRAP = 12;
+		public static final int CENTER_STAGE_RED_TRAP = 13;
+		public static final int CENTER_STAGE_BLUE_TRAP = 14;
+		public static final int STAGE_LEFT_BLUE_TRAP = 15;
+		public static final int STAGE_RIGHT_BLUE_TRAP = 16;
 	
 		public APRILTAGS() {
 		}
