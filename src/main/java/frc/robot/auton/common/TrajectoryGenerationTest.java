31a1b9e330aa3ee62040974e6bf46d75099db63c
Mr. A <GabrielArtaud@comcast.net>
2024-03-03 17:23:19 -0500

    added better example on how to test trajectory generation

--- src/main/java/frc/robot/auton/common/TrajectoryGenerationTest.java
+++ src/main/java/frc/robot/auton/common/TrajectoryGenerationTest.java
@@ -1,17 +1,24 @@
 package frc.robot.auton.common;
 
 
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.*;
 import frc.robot.interfaces.*;
-
+import frc.robot.auton.AutonConstants;
 import frc.robot.auton.sp1.*;
 import frc.robot.auton.sp2.*;
 import frc.robot.auton.sp3.*;
 import frc.robot.auton.sp4.*;
 import frc.robot.auton.sp6.*;
+import frc.robot.commands.drivetrain.DrivetrainSwerveRelative;
 
 // GP = game piece
 // Can be used to place one cube or one cone and either starting position one or two
@@ -22,9 +29,11 @@ public class TrajectoryGenerationTest extends SequentialCommandGroup {
 		addCommands(
 
 			// replace line below by whatever trajectory-generation command you want to test
-			new StartingPositionTwoDriveAndTurnToLeftNote(drivetrain, container, object_detection_camera)
+			//new StartingPositionTwoDriveAndTurnToLeftNote(drivetrain, container, object_detection_camera)
+
+			// another example, calling a trajectory direcly:
+			new DrivetrainSwerveRelative(drivetrain, container, StartingPositionOnePickupSecondNote.createPickupSecondNoteTrajectory(container))
 		); 
   
 	}
-
 }
