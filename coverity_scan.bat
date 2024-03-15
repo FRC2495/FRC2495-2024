d1f65c63114e079eacf15e08477b1359ec3065d4
Mr. A <GabrielArtaud@comcast.net>
2024-03-14 20:52:43 -0400

    Added reminder on how to run Coverity against PhotonVision for Orange Pi 5 for future reference

--- coverity_scan.bat
+++ coverity_scan.bat
@@ -1,1 +1,4 @@
  C:\temp\cov-analysis-win64-2023.6.2\bin\cov-build --dir cov-int gradlew build   -Dorg.gradle.java.home="C:\Users\Public\wpilib\2024\jdk"
+
+rem for PhotonVision for Orange Pi 5:
+rem C:\temp\cov-analysis-win64-2023.6.2\bin\cov-build --dir cov-int gradlew shadowJar -PArchOverride=linuxarm64
